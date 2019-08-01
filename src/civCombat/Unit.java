2cb664e3e59a0854b4f9f9d4b2403c0d4d93410b
Doug <Doug@DESKTOP-6JAF9FK>
2019-08-01 15:21:45 +0100

    Implemented toString, equals, hashCode for Unit

--- src/civCombat/Unit.java
+++ src/civCombat/Unit.java
@@ -65,6 +65,44 @@ public abstract class Unit {
         System.out.print("Attack: " + this.baseAttack + " ");
         System.out.print("Health: " + this.baseHealth + " ");
     }
-   
+
+    @Override
+    public String toString() {
+        return "Unit{" + "baseAttack=" + baseAttack + ", baseHealth=" + baseHealth + ", wounds=" + wounds + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + this.baseAttack;
+        hash = 41 * hash + this.baseHealth;
+        hash = 41 * hash + this.wounds;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Unit other = (Unit) obj;
+        if (this.baseAttack != other.baseAttack) {
+            return false;
+        }
+        if (this.baseHealth != other.baseHealth) {
+            return false;
+        }
+        if (this.wounds != other.wounds) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
